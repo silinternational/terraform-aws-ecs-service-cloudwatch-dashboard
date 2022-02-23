08e74afef63706b371cf406ff716ebedaa3b6c79
Schparky <3172830+Schparky@users.noreply.github.com>
2022-02-23 10:01:34 -0700

    refactor - change templates to for loop with jsonencode

--- main.tf
+++ main.tf
@@ -1,14 +1,8 @@
 resource "aws_cloudwatch_dashboard" "main" {
   dashboard_name = var.dashboard_name
-  dashboard_body = data.template_file.dashboard_body.rendered
-}
-
-data "template_file" "dashboard_body" {
-  template = file("${path.module}/dashboard-template.json")
-
-  vars = {
-    widgets_json = join(",", data.template_file.widgets.*.rendered)
-  }
+  dashboard_body = jsonencode({
+    widgets = local.widgets
+  })
 }
 
 data "template_file" "widgets" {
@@ -20,4 +14,46 @@ data "template_file" "widgets" {
     cluster_name = var.cluster_name
     service_name = element(var.service_names, count.index)
   }
+}
+
+locals {
+  widgets = [for service_name in var.service_names : {
+    type   = "metric"
+    width  = 18
+    height = 6
+    properties = {
+      view    = "timeSeries"
+      stacked = false
+      metrics = [
+        ["AWS/ECS", "CPUUtilization", "ServiceName", service_name, "ClusterName", var.cluster_name, { color = "#d62728", stat = "Maximum" }],
+        [".", "MemoryUtilization", ".", ".", ".", ".", { yAxis = "right", color = "#1f77b4", stat = "Maximum" }]
+      ]
+      region = var.aws_region,
+      annotations = {
+        horizontal = [
+          {
+            color = "#ff9896",
+            label = "100% CPU",
+            value = 100
+          },
+          {
+            color = "#9edae5",
+            label = "100% Memory",
+            value = 100,
+            yAxis = "right"
+          },
+        ]
+      }
+      yAxis = {
+        left = {
+          min = 0
+        }
+        right = {
+          min = 0
+        }
+      }
+      title  = "${var.cluster_name} / ${service_name}"
+      period = 300
+    }
+  }]
 }
