ad197e12b981e3c43f138dc8b14e8c50e19c1ba7
briskt <3172830+briskt@users.noreply.github.com>
2023-06-01 12:51:23 +0800

    remove aws_region from example

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,5 @@
 module "dashboard" {
   source         = "../"
-  aws_region     = var.aws_region
   cluster_name   = "a-cluster"
   dashboard_name = "my-dashboard"
   service_names  = ["service-1", "service-2"]
